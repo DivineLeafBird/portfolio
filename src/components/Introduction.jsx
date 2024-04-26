a51f449f784040ca0398fd15873a3883a139400a
DivineLeafBird <kassimyahya93@gmail.com>
2024-04-26 15:33:21 +0300

    Update Introduction and Roles components

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,11 +6,11 @@ const Introduction = () => {
     <>
       <section className="gradientBg text-tertiary px-5 py-20 max-w-screen-2xl mx-auto">
         <div className="flex flex-col-reverse md:flex-row items-center justify-center">
-          <div className="container mx-auto w-1/2">
-            <h1 className="text-2xl font-semibold mt-5">
+          <div className="container mx-auto md:w-1/2">
+            <h1 className="text-2xl font-semibold mt-5 text-center md:text-left ">
               Hi, I'm <span className="text-pink">Kassim</span>
             </h1>
-            <p className="text-lg mt-5">
+            <p className="text-lg mt-5 text-center md:text-left">
               Software Engineer, <br />
               IT Support Specialist, and UX / UI Designer.
             </p>
--- src/components/Roles.jsx
+++ src/components/Roles.jsx
@@ -13,30 +13,30 @@ const Roles = () => {
         <div className="flex flex-col md:flex-row justify-center items-center md:gap-12 gap-8 text-center">
           <div className="bg-pink md:w-1/4 rounded-[35px] px-5 py-8 space-y-4 shadow-3xl items-center flex flex-col justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
             <img src={Developer} alt="" className="z-auto" />
-            <h5 className="text-2xl font-semibold text-primary">
+            <h5 className="text-lg font-medium text-primary">
               Software Engineer{" "}
             </h5>
-            <p className="text-lg text-secondary">
+            <p className="text-base text-secondary">
               "Problem solver by day, code whisperer by night. Let's craft some
               innovative solutions."
             </p>
           </div>
           <div className="bg-pink md:mt-24 md:w-1/4 rounded-[35px] px-5 py-8 space-y-4 shadow-3xl items-center flex flex-col justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
             <img src={Support} alt="" className="z-auto" />
-            <h5 className="text-2xl font-semibold text-primary">
+            <h5 className="text-lg font-medium text-primary">
               IT Support Specialist{" "}
             </h5>
-            <p className="text-lg text-secondary">
+            <p className="text-base text-secondary">
               "Keeping your tech running smoothly is my superpower. IT support
               at your service."
             </p>
           </div>
           <div className="bg-pink md:w-1/4 rounded-[35px] px-5 py-8 space-y-4 shadow-3xl  items-center flex flex-col justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer">
             <img src={Designer} alt="" className="z-auto" />
-            <h5 className="text-2xl font-semibold text-primary">
+            <h5 className="text-lg font-medium text-primary">
               UX / UI Designer{" "}
             </h5>
-            <p className="text-lg text-secondary">
+            <p className="text-base text-secondary">
               "I design interfaces that delight. I craft user experiences that
               are both intuitive and stunning."
             </p>
